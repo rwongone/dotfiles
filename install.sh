aa8aa7db1c6d6a456ad8d0f89758bb6da367040d
Richard Wong <richard.wong.com@gmail.com>
2018-03-27 21:04:26 -0400

    scripts to install and update configs

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+mkdir -p ~/.config
+cp -R nvim ~/.config/nvim 
+ln tmux/.tmux.conf ~/.tmux.conf 
+ln zsh/.zshrc ~/.zshrc
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+cp -R ~/.config/nvim/init.vim nvim
+cp -R ~/.config/nvim/vimrc nvim/vimrc
