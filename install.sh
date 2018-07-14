ac78d4c2f7f098b17e6305cbfb64dd969d36fd2d
Richard Wong <richard.wong.com@gmail.com>
2018-07-14 15:09:43 -0400

    Use vimdiff as diff tool

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Move nvim directories into the right place
-mkdir -p ~/.config
+mkdir -p ~/.config ~/bin
 cp -R nvim/autoload ~/.config/nvim/
 cp -R nvim/colors ~/.config/nvim/
 
@@ -10,3 +10,4 @@ ln -b nvim/init.vim ~/.config/nvim/init.vim
 ln -b tmux/.tmux.conf ~/.tmux.conf
 ln -b zsh/.zshrc ~/.zshrc
 ln -b git/.gitconfig ~/.gitconfig
+ln -b git/git_diff_wrapper ~/bin/git_diff_wrapper
