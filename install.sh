21ec39ad2e27530efd718cb30acd9d36b1e7fd17
Richard Wong <rwong@wealthsimple.com>
2021-10-04 17:03:32 -0400

    Add ideavim

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ mkdir -p ~/.config ~/bin
 cp -R nvim/autoload ~/.config/nvim/
 cp -R nvim/colors ~/.config/nvim/
 
-# Back up hardlink destinations as filename~
+# Back up hardlink destinations as filename
 ln -f nvim/init.vim ~/.config/nvim/init.vim
 ln -f tmux/.tmux.conf ~/.tmux.conf
 ln -f zsh/.zshrc ~/.zshrc
 ln -f git/.gitconfig ~/.gitconfig
 ln -f git/git_diff_wrapper ~/bin/git_diff_wrapper
+ln -f ideavim/.ideavim ~/.ideavim
