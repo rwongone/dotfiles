87722ccdda04050bceb9edcc655e0394ce380768
Richard Wong <rwong@wealthsimple.com>
2022-04-20 14:49:53 -0400

    Use default git diff. Add some extra installation steps

--- install.sh
+++ install.sh
@@ -12,3 +12,15 @@ ln -f zsh/.zshrc ~/.zshrc
 ln -f git/.gitconfig ~/.gitconfig
 ln -f git/git_diff_wrapper ~/bin/git_diff_wrapper
 ln -f ideavim/.ideavimrc ~/.ideavimrc
+
+# Set up .z.sh
+curl https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.z.sh
+
+# Set up asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf plugin-add python
+
+# Install ag (the_silver_searcher)
+brew install ag
