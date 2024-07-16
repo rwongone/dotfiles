e6670264782d89f00c97b8939836a8e966e319bf
Richard Wong <rwong@wealthsimple.com>
2024-07-16 16:18:17 -0400

    open-browser-github

---
+++ nvim/lua/rwong/plugins/open-browser-github.lua
@@ -0,0 +1,11 @@
+return {
+  "tyru/open-browser-github.vim",
+  dependencies = {
+    "tyru/open-browser.vim"
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>gp", "<Cmd>OpenGithubProject<CR>", { desc = "Open GitHub project" })
+    vim.keymap.set("n", "<leader>gf", "<Cmd>OpenGithubFile<CR>", { desc = "Open GitHub file" })
+    vim.keymap.set("v", "<leader>gf", "<Cmd>OpenGithubFile<CR>", { desc = "Open GitHub file" })
+  end
+}
