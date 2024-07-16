094e755fd8d3a7751ab1a104d990ab73cbb473a8
Richard Wong <rwong@wealthsimple.com>
2024-07-16 17:00:51 -0400

    terminal movement

--- nvim/lua/rwong/core/keymaps.lua
+++ nvim/lua/rwong/core/keymaps.lua
@@ -12,6 +12,10 @@ keymap.set("i", "<ESC>", "<nop>", { desc = "Disable <ESC> in insert mode" })
 keymap.set("n", "<F1>", "<nop>", { desc = "Disable F1" })
 keymap.set("n", "Q", "<nop>", { desc = "Disable Q" })
 keymap.set("n", "K", "<nop>", { desc = "Disable K" })
+keymap.set("n", "<Right>", "<nop>", { desc = "Disable <Right>" })
+keymap.set("n", "<Left>", "<nop>", { desc = "Disable <Left>" })
+keymap.set("n", "<Up>", "<nop>", { desc = "Disable <Up>" })
+keymap.set("n", "<Down>", "<nop>", { desc = "Disable <Down>" })
 
 -- use ; instead of : for command mode
 keymap.set("n", ";", ":", { desc = "Use ; instead of :" })
@@ -34,7 +38,10 @@ keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" }) -- move to l
 keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" }) -- move to bottom split
 keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" }) -- move to top split
 keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" }) -- move to right split
-
+keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h", { desc = "Move to left split from a terminal" }) -- move to left split
+keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j", { desc = "Move to bottom split from a terminal" }) -- move to bottom split
+keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k", { desc = "Move to top split from a terminal" }) -- move to top split
+keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l", { desc = "Move to right split from a terminal" }) -- move to right split
 
 keymap.set("n", "<leader>n", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>w", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
