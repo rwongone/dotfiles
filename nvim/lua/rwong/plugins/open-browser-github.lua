d771d621f6cf89fdc16881eab89d13e61beae702
Richard Wong <rwong@wealthsimple.com>
2024-07-23 10:29:04 -0400

    fix opening github files at specific line

--- nvim/lua/rwong/plugins/open-browser-github.lua
+++ nvim/lua/rwong/plugins/open-browser-github.lua
@@ -6,6 +6,6 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>gp", "<Cmd>OpenGithubProject<CR>", { desc = "Open GitHub project" })
     vim.keymap.set("n", "<leader>gf", "<Cmd>OpenGithubFile<CR>", { desc = "Open GitHub file" })
-    vim.keymap.set("v", "<leader>gf", "<Cmd>OpenGithubFile<CR>", { desc = "Open GitHub file" })
+    vim.keymap.set("v", "<leader>gf", ":OpenGithubFile<CR>", { desc = "Open GitHub file" })
   end
 }
