09e0a6a30ad6d91a364d87ab247e275f38f4e538
Richard Wong <rwong@wealthsimple.com>
2026-01-08 17:28:16 -0500

    enable noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,6 +1,10 @@
 -- ABOUTME: Disable noice.nvim - it was causing vim to exit when typing : or ;
 -- ABOUTME: Using default vim cmdline instead
 return {
-  "folke/noice.nvim",
-  enabled = false,
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = { enabled = true },
+    },
+  },
 }
